6004aabbbb7842415f1857d7e49e1cfdc581c53e
Yuri Souza <101481299+Souza1999@users.noreply.github.com>
2023-05-11 10:00:26 -0300

    Variável declarada sem o comando global

---
+++ Aulas/Aula 17: variáveis locais e globais/1_SemGlobal.py
@@ -0,0 +1,17 @@
+def soma (numero1, numero2):
+  n1 = 100
+  resposta = numero1 + numero2 + n1
+  return resposta
+
+# ----------------------------------------
+
+n1 = int(input("Diga o 1º número: ")))
+n2 = int(input("Diga o 2º número: ")))
+saida = soma(n1,n2)
+
+print(f"\nResultado: {saida}"
+      f"\n(n1 = {n1})")
+
+'''
+  
+'''
