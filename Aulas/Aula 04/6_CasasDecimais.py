ad409c5c5ddac746d33d4ed6ea119eafaf6e458a
Yuri Souza <101481299+Souza1999@users.noreply.github.com>
2023-05-22 00:02:07 -0300

    Casas decimais com espaços preestabelecidos.

--- Aulas/Aula 04/6_CasasDecimais.py
+++ Aulas/Aula 04/6_CasasDecimais.py
@@ -1,5 +1,5 @@
-Dolar = 5.1433
+Dolar = 2.1433
 print (f'{Dolar: 10.3f}')
 
-# Esse número antes do ponto significa um total de espaços, caracteres e espaços em brancos misturas.
-# Ou seja, nesse caso, o print será:     5.143
+# Esse número antes do ponto significa um total de espaços (espaços em brancos e caracteres misturados).
+# Ou seja, nesse caso, o print será:     2.143
