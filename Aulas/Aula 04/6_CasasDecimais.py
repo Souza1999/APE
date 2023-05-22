5f5beb57e6e5cfa9caea11d809ebdd0dabdec1d7
Yuri Souza <101481299+Souza1999@users.noreply.github.com>
2023-05-21 23:59:08 -0300

    Create 6_CasasDecimais.py

---
+++ Aulas/Aula 04/6_CasasDecimais.py
@@ -0,0 +1,5 @@
+Dolar = 5.1433
+print (f'{Dolar: 10.3f}')
+
+# Esse número antes do ponto significa um total de espaços, caracteres e espaços em brancos misturas.
+# Ou seja, nesse caso, o print será:     5.143
