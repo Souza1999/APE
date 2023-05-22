956e8aaf9b4ed4bfcba59b167a7db5d1b8fa19e2
Yuri Souza <101481299+Souza1999@users.noreply.github.com>
2023-05-22 00:03:44 -0300

    Adicionando mais uma variável

--- Aulas/Aula 04/6_CasasDecimais.py
+++ Aulas/Aula 04/6_CasasDecimais.py
@@ -3,3 +3,6 @@ print (f'{Dolar: 10.3f}')
 
 # Esse número antes do ponto significa um total de espaços (espaços em brancos e caracteres misturados).
 # Ou seja, nesse caso, o print será:     2.143
+
+Euler = 2.71828
+print (f'{Euler: 15.2f}')
